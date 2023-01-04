aa9c2d6fab3e8697473d9fd29c7a229fd2d9bf99
Ryan Carr <ryan.g.carr@hotmail.com>
2023-01-03 22:00:56 -0500

    adding functioning counter

--- js/main.js
+++ js/main.js
@@ -1,24 +1,38 @@
 // Variables
 let score = 0;
 
+let currVal;
+
 // Cached Elements
 
 let addOne = document.querySelector("#add");
 let subOne = document.querySelector("#subtract");
-let valInp = document.getElementById("valInput").value;
 let counter = document.querySelector("#count-main");
 
 // Event Listeners
 
 addOne.addEventListener("click", function () {
+  getVal();
   addVal();
+  counter.innerHTML = score;
+});
+
+subOne.addEventListener("click", function () {
+  getVal();
+  subVal();
+  counter.innerHTML = score;
 });
 
 // Functions
 
 function addVal() {
-  //   score = score + valInp;
-  console.log(valInp);
-  score = score + valInp;
-  counter.innerHTML = score;
+  score = score + parseInt(currVal);
+}
+
+function subVal() {
+  score = score - parseInt(currVal);
+}
+
+function getVal() {
+  currVal = document.getElementById("valInput").value;
 }
